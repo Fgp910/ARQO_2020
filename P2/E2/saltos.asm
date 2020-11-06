63d2603fba6af8dec30210203350647e12b3366c
leandroGO <leandrogogi19@gmail.com>
2020-11-06 15:43:33 +0100

    Comentarios

--- P2/E2/saltos.asm
+++ P2/E2/saltos.asm
@@ -34,9 +34,9 @@ main:
   # Load antes de branch
   salto1:
   lw $s0, 4($zero)
-  beq $s0, $zero, salto2 # No salta
+  beq $s0, $zero, salto2 # No salta (se detiene 1 ciclo)
   lw $s1, 4($zero)
-  beq $s1, $s1, salto2   # Salta
+  beq $s1, $s1, salto2   # Salta (se detiene 1 ciclo)
   add $t4, $t4, $t4      # No se debe ejecutar
   add $t4, $t4, $t4      # No se debe ejecutar
   add $t4, $t4, $t4      # No se debe ejecutar
