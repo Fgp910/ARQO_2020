2383411e32f02428b8c2f46f037299a1b563df8b
Fgp910 <f.gutierrezpena910@gmail.com>
2020-11-27 21:09:07 +0100

    Creo que ya está la parte de los tiempos. Falta lo de cachegrind, escribir en fichero todo y generar las gráficas

--- P3/mult_time.sh
+++ P3/mult_time.sh
@@ -1,24 +1,25 @@
 # ARQO 2020-2021. Task 3: exercise 3
-# Script for times and plot generation (mult, mult_trans)
+# Script for times generation, cachegrind simulation and the corresponding plots
 
 #!/bin/bash
 
 # inicializar variables
 P=5
-Ninicio=$((1000 + 1024 * $P))
-Npaso=64
-Nfinal=$((1000 + 1024 * ($P + 1)))
+Ninicio=$((256 + 256 * $P))
+Npaso=32
+Nfinal=$((256 + 256 * ($P + 1)))
 Iter=3
-fDAT=time_slow_fast.dat
-fPNG=time_slow_fast.png
+fDAT=mult.dat
+fPNG_cache=mult_cache.png
+fPNG_time=mult_time.png
 
 # borrar el fichero DAT y el fichero PNG
-rm -f $fDAT fPNG
+rm -f $fDAT $fPNG_cache $fPNG_time
 
 # generar el fichero DAT vacío
 touch $fDAT
 
-echo "Running slow and fast..."
+echo "Running mult and mult_trans..."
 # bucle para N desde P hasta Q 
 #for N in $(seq $Ninicio $Npaso $Nfinal);
 unset slowTime
@@ -28,13 +29,13 @@ for i in $(seq $Ninicio $Npaso $Nfinal); do fastTime+=(0); done
 for ((i=1; i <= Iter; i++)); do
     echo "I: $i / $Iter..."
     for ((N=Ninicio, j=0; N <= Nfinal ; N+=Npaso, j++)); do
-        echo -e "\tslow, N: $N / $Nfinal..."
-        aux=$(./slow $N | grep 'time' | awk '{print $3}')
+        echo -e "\tmult, N: $N / $Nfinal..."
+        aux=$(./mult $N | grep 'time' | awk '{print $3}')
         slowTime[$j]=$(echo "${slowTime[$j]} + $aux" | bc)
     done
     for ((N=Ninicio, j=0; N <= Nfinal ; N+=Npaso, j++)); do
-        echo -e "\tfast, N: $N / $Nfinal..."
-        aux=$(./fast $N | grep 'time' | awk '{print $3}')
+        echo -e "\tmult_trans, N: $N / $Nfinal..."
+        aux=$(./mult_trans $N | grep 'time' | awk '{print $3}')
         fastTime[$j]=$(echo "${fastTime[$j]} + $aux" | bc)
     done
 done
@@ -56,8 +57,8 @@ set key right bottom
 set grid
 set term png
 set output "$fPNG"
-plot "$fDAT" using 1:2 with lines lw 2 title "slow", \
-     "$fDAT" using 1:3 with lines lw 2 title "fast"
+plot "$fDAT" using 1:2 with lines lw 2 title "mult", \
+     "$fDAT" using 1:3 with lines lw 2 title "mult_trans"
 replot
 quit
 END_GNUPLOT
