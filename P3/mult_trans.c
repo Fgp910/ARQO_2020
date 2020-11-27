608affb1b5aa94559474090cd955acab2708a1cb
Fgp910 <f.gutierrezpena910@gmail.com>
2020-11-27 12:11:31 +0100

    Ya es diferente, es como quiere ser, libre

--- P3/mult_trans.c
+++ P3/mult_trans.c
@@ -5,7 +5,7 @@
 
 #include "arqo3.h"
 
-int mainCleanUp(tipo**, tipo**, tipo**, int);
+int mainCleanUp(tipo**, tipo**, tipo**, tipo**, int);
 
 int main( int argc, char *argv[])
 {
@@ -34,21 +34,34 @@ int main( int argc, char *argv[])
 	B=generateMatrix(n);
 	if( !B )
 	{
-		return mainCleanUp(A, NULL, NULL, -1);
+		return mainCleanUp(A, NULL, NULL, NULL, -1);
+	}
+	Bt=generateEmptyMatrix(n);
+	if( !Bt )
+	{
+		return mainCleanUp(A, B, NULL, NULL, -1);
 	}
 	C=generateEmptyMatrix(n);
 	if( !C )
 	{
-		return mainCleanUp(A, B, NULL, -1);
+		return mainCleanUp(A, B, Bt, NULL, -1);
 	}
 
 	gettimeofday(&ini,NULL);
 
 	/* Main computation */
+    /* B transposition */
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            Bt[i][j] = B[j][i];
+        }
+    }
+
+    /* Actual multiplication*/
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
             for (k = 0; k < n; j++) {
-                C[i][j] += A[i][k] * B[k][j];
+                C[i][j] += A[i][k] * Bt[j][k];
             }
         }
     }
@@ -56,13 +69,21 @@ int main( int argc, char *argv[])
 
 	gettimeofday(&fin,NULL);
 	printf("Execution time: %f\n", ((fin.tv_sec*1000000+fin.tv_usec)-(ini.tv_sec*1000000+ini.tv_usec))*1.0/1000000.0);
+	printf("Result:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%lf\t", C[i][j]);
+        }
+        printf("\n");
+    }
 
-	return mainCleanUp(A, B, C, 0);
+	return mainCleanUp(A, B, Bt, C, 0);
 }
 
-int mainCleanUp(tipo** A, tipo** B, tipo** C, int retVal) {
-    if (A) free(A);
-    if (B) free(B);
-    if (C) free(C);
+int mainCleanUp(tipo** A, tipo** B, tipo** Bt, tipo** C, int retVal) {
+    if (A)  free(A);
+    if (B)  free(B);
+    if (Bt) free(Bt);
+    if (C)  free(C);
     return retVal;
 }
