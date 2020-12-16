bd9d289723c22ddea2b4943b5e7fc333cd382616
LeandroGO <leandrogogi19@gmail.com>
2020-12-16 08:41:33 -0800

    A ver si cuela

--- P4/time_serie_par.sh
+++ P4/time_serie_par.sh
@@ -3,54 +3,63 @@
 #!/bin/bash
 
 # Variables
-C=
-M=$(2*$C)
-NInicio=
-NPaso=
-NFinal=
-Iter=5
+C=4
+M=$((2 * $C))
+NInicio=5000000
+NPaso=6000000
+NFinal=40000000
+Iter=4
 fDat=timeSeriePar.dat
 
 # Execution block
 unset tSerie
+unset tPar
+for i in $(seq $NInicio $NPaso $NFinal); do tSerie+=(0); done
 for k in $(seq 2 1 $M); do
-    unset tPar$k
+    for i in $(seq $NInicio $NPaso $NFinal); do tPar+=(0); done
 done
-for i in $(seq $Ninicio $Npaso $Nfinal); do tSerie+=(0); done
-for i in $(seq $Ninicio $Npaso $Nfinal); do tPar+=(0); done
 for ((i=1; i<=$Iter; i++)); do
-    echo "I $i/$Iter"
-    for ((j=0, N=$NInicio; N<=$NFinal; j+=1, N+=$NPaso)); do
+    echo "I $i / $Iter"
+    for ((N=$NInicio, j=0; N<=NFinal; N+=NPaso, j++)); do
         aux=$(./pescalar_serie $N | grep 'Tiempo:' | awk '{print $2}')
         tSerie[$j]=$(echo "${tSerie[$j]} + $aux" | bc)
-        for k in $(seq 2 1 $M); do
-            export OMP_NUM_THREADS=$k
-            aux=$(./pescalar_par3 $N | grep 'Tiempo:' | awk '{print $2}')
-            tPar$k[$j]=$(echo "${tPar$k[$j]} + $aux" | bc)
+        for k in $(seq 0 1 $(($M-2))); do
+            export OMP_NUM_THREADS=$(($k + 2))
+            aux=$(./pescalar_par3 $N $(($k + 2)) | grep 'Tiempo:' | awk '{print $2}')
+            tPar[$((($j - 1)*($M - 1) + $k))]=$(echo "${tPar[$((($j - 1)*($M - 1) + $k))]} + $aux" | bc)
         done
     done
 done
 
+rm -rf $fDat
 for ((j=0, N=$NInicio; N<=$NFinal; j+=1, N+=$NPaso)); do
     serieMean=$(echo "${tSerie[$j]}  / $Iter" | bc -l)
     str="$N    $serieMean"
     for k in $(seq 2 1 $M); do
-        parMean=$(echo "${tPar$k[$j]}  / $Iter" | bc -l)
-        str=$str+"   $parMean"
+        parMean=$(echo "${tPar[$((($j - 1)*($M - 1) + $k))]}  / $Iter" | bc -l)
+        str="$str   $parMean"
     done
+    echo $str >> $fDat
 done
 
 # Plot generation
 echo "Generating plots..."
 gnuplot << END_GNUPLOT
-set title "Scalar product execution time depending on number of threads"
+set title "Scalar Product Execution Time Depending on Number of Threads"
 set xlabel "Vector size"
 set ylabel "Time (s)"
 set grid 
-set term PNG
+set term png
+set key outside right center
 set output "pescalar.png"
 plot "$fDat" u 1:2 w l title "Serial", \
-     for [i=2:$M] "$fDat" u 1:($1+i) w l title i." threads"
+     "$fDat" u 1:3 w l title "2 threads", \
+     "$fDat" u 1:4 w l title "3 threads", \
+     "$fDat" u 1:5 w l title "4 threads", \
+     "$fDat" u 1:6 w l title "5 threads", \
+     "$fDat" u 1:7 w l title "6 threads", \
+     "$fDat" u 1:8 w l title "7 threads", \
+     "$fDat" u 1:9 w l title "8 threads"
 replot
 quit
 END_GNUPLOT
