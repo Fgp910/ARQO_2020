26267599cfd833cf43a779d0abcb9043fd7d3f7e
leandroGO <leandrogogi19@gmail.com>
2020-12-18 19:49:06 +0100

    miabuela

--- P4/time_serie_par.sh
+++ P4/time_serie_par.sh
@@ -3,19 +3,18 @@
 #!/bin/bash
 
 # Variables
-C=4
-M=$((2 * $C))
-NInicio=10000000
-NPaso=15000000
-NFinal=100000000
-Iter=10
+M=9
+NInicio=1000000
+NPaso=1500000
+NFinal=10000000
+Iter=1
 fDat=timeSeriePar.dat
 
 # Execution block
 unset tSerie
 unset tPar
 for i in $(seq $NInicio $NPaso $NFinal); do tSerie+=(0); done
-for k in $(seq 2 1 $M); do
+for k in $(seq 1 1 $M); do
     for i in $(seq $NInicio $NPaso $NFinal); do tPar+=(0); done
 done
 for ((i=1; i<=$Iter; i++)); do
@@ -23,10 +22,10 @@ for ((i=1; i<=$Iter; i++)); do
     for ((N=$NInicio, j=0; N<=NFinal; N+=NPaso, j++)); do
         aux=$(./pescalar_serie $N | grep 'Tiempo:' | awk '{print $2}')
         tSerie[$j]=$(echo "${tSerie[$j]} + $aux" | bc)
-        for k in $(seq 0 1 $(($M-2))); do
-            export OMP_NUM_THREADS=$(($k + 2))
-            aux=$(./pescalar_par3 $N $(($k + 2)) | grep 'Tiempo:' | awk '{print $2}')
-            tPar[$(($j*($M - 1) + $k))]=$(echo "${tPar[$(($j*($M - 1) + $k))]} + $aux" | bc)
+        for k in $(seq 1 1 $M); do
+            export OMP_NUM_THREADS=$(($k*2))
+            aux=$(./pescalar_par3 $N | grep 'Tiempo:' | awk '{print $2}')
+            tPar[$(($j*$M + ($k-1)))]=$(echo "${tPar[$(($j*$M + ($k-1)))]} + $aux" | bc)
         done
     done
 done
@@ -35,8 +34,8 @@ rm -rf $fDat
 for ((j=0, N=$NInicio; N<=$NFinal; j+=1, N+=$NPaso)); do
     serieMean=$(echo "${tSerie[$j]}  / $Iter" | bc -l)
     str="$N    $serieMean"
-    for k in $(seq 0 1 $(($M-2))); do
-        parMean=$(echo "${tPar[$(($j*($M - 1) + $k))]}  / $Iter" | bc -l)
+    for k in $(seq 1 1 $M); do
+        parMean=$(echo "${tPar[$(($j*$M + ($k-1)))]}  / $Iter" | bc -l)
         str="$str   $parMean"
     done
     echo $str >> $fDat
@@ -54,12 +53,14 @@ set key outside right center
 set output "pescalar.png"
 plot "$fDat" u 1:2 w l title "Serial", \
      "$fDat" u 1:3 w l title "2 threads", \
-     "$fDat" u 1:4 w l title "3 threads", \
-     "$fDat" u 1:5 w l title "4 threads", \
-     "$fDat" u 1:6 w l title "5 threads", \
-     "$fDat" u 1:7 w l title "6 threads", \
-     "$fDat" u 1:8 w l title "7 threads", \
-     "$fDat" u 1:9 w l title "8 threads"
+     "$fDat" u 1:4 w l title "4 threads", \
+     "$fDat" u 1:5 w l title "6 threads", \
+     "$fDat" u 1:6 w l title "8 threads", \
+     "$fDat" u 1:7 w l title "10 threads", \
+     "$fDat" u 1:8 w l title "12 threads", \
+     "$fDat" u 1:9 w l title "14 threads", \
+     "$fDat" u 1:10 w l title "16 threads", \
+     "$fDat" u 1:11 w l title "18 threads"
 replot
 quit
 END_GNUPLOT
